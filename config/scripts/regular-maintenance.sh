c2fed7c383166c515d0f66b67f9225dc84fe5bcf
Ryan J. Price <ryapric@gmail.com>
2023-03-05 19:09:41 -0600

    Add some systemd stuff for regular maintenance

---
+++ config/scripts/regular-maintenance.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd /home/admin/config || exit 1
+
+# Tear down Docker Compose stack completely, and restart. Note that we're
+# backing up resolv.conf because otherwise DNS will go down once Pi-hole does
+mv /etc/resolv.conf{,.bak}
+printf 'nameserver 9.9.9.9\n' > /etc/resolv.conf
+# TODO: once the stack has more than just Pi-hole in it, maybe be a bit less
+# nuclear here
+docker compose down
+docker system prune --force
+docker volume ls --format '{{ .Name }}' | grep pihole | xargs -I{} docker volume rm {}
+docker compose up -d --wait
+mv /etc/resolv.conf{.bak,}
+docker exec -it pihole sh -c 'pihole -g'
