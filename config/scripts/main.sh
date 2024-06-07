91c51f8b93ae088ce4265164985adf0d03f7af63
Ryan J. Price <ryapric@gmail.com>
2024-06-07 01:54:41 -0500

    Get most things working -- systemd service fails but the script itself runs fine?

--- config/scripts/main.sh
+++ config/scripts/main.sh
@@ -8,7 +8,7 @@ fi
 
 host="${1:-localhost}"
 ansible_flags="--inventory ${host},"
-if [[ "${host}" == 'localhost' ]] ; then
+if [[ "${host}" == 'localhost' || "${host}" == '127.0.0.1' ]] ; then
   ansible_flags="${ansible_flags} --connection local"
 fi
 
@@ -18,61 +18,10 @@ apt-get update && apt-get install -y \
   make \
   sudo
 
-ansible-galaxy role install \
-  geerlingguy.docker
-
 src="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+ansible-galaxy install -r "${src}/ansible/requirements.yaml"
 ansible-lint "${src}/ansible/main.yaml"
 # The next line doesn't quote the flags var because it intentionally needs to be
 # multiple tokens
 # shellcheck disable=SC2086
 ansible-playbook ${ansible_flags} "${src}/ansible/main.yaml"
-
-# printf '>>> Bringing up services defined in docker-compose.yaml...\n'
-# cmpfile="${cfg_dir}/docker-compose.yaml"
-# docker compose -f "${cmpfile}" pull
-# docker compose -f "${cmpfile}" up -d --wait
-# docker system prune --volumes --force
-
-# printf '>>> Setting up systemd services...\n'
-
-# cat <<EOF > /etc/systemd/system/regular-maintenance.service
-# [Unit]
-# Description=Periodic maintenance tasks for home-stuff services
-# Wants=regular-maintenance.timer
-
-# [Service]
-# ExecStart=${cfg_dir}/scripts/regular-maintenance.sh
-# Type=oneshot
-
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-
-# cat <<EOF > /etc/systemd/system/regular-maintenance.timer
-# [Unit]
-# Description=Daily maintenance tasks for home-stuff services
-
-# [Timer]
-# Unit=regular-maintenance.service
-# OnBootSec=30s
-# OnCalendar=Sun *-*-* 04:*:*
-# AccuracySec=1s
-# Persistent=true
-
-# [Install]
-# WantedBy=timers.target
-# EOF
-
-# systemctl daemon-reload
-# systemctl enable regular-maintenance.timer
-# systemctl start regular-maintenance.timer
-
-# # TODO: if you want to add adlists URLs, you can manually insert relevant
-# # records into the gravity DB one at a time, like so (in v5+):
-# #     docker exec pihole sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled, comment) VALUES ('${adlist_url}', 1, '${adlist_comment}');"
-# # which you can run in a loop where you 'docker exec' each one you want. You'll
-# # also then need to update gravity once they're all added:
-# #     docker exec pihole pihole -g
-
-# printf '>>> All done!\n'
